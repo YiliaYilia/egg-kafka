201703b973c0a0014031a68a123342836a8b5b30
shaddock_hu <shaddock_hu@adsame.com>
2017-02-14 12:27:47 +0800

    add test

---
+++ test/kafka.test.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const assert = require('assert');
+describe('test/kafka.test.js', () => {
+  it('test connection', function* () {
+    const app = true;
+    assert(app === true);
+  });
+});
